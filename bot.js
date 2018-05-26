040e5f802a15b680292ec43f7fb05371374074fd
jlsajfj <jlsajfj@hotmail.com>
2018-05-26 11:33:00 -0500

    a

--- bot.js
+++ bot.js
@@ -15,35 +15,5 @@ client.on('message', msg => {
 		msg.channel.send(x);
 	}
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-// console.log(bot.username);
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            case 'Test':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'HELLO WORLD!!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+
 client.login(auth.token);
