b898d2b94d7b84cc32726a4cba4997e0946d3a0d
jlsajfj <jlsajfj@hotmail.com>
2018-05-26 11:56:29 -0500

    a

--- bot.js
+++ bot.js
@@ -4,10 +4,14 @@ const DL = require('detectlanguage');
 var ld = new LD();
 var auth = require('./auth.json');
 const client = new Discord.Client();
-var detectLanguage = new DL({
+var dl = new DL({
 	key:'c6378c3e5bf306205bdee72bc263b5d2'
 });
-
+var a;
+dl.languages(function(error,result){
+	a=result
+	console.log(a);
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -15,14 +19,21 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	if(msg.author.id!=client.user.id){
-		detectLanguage.detect(msg.content, function(error, result) {
+		dl.detect(msg.content, function(error, result) {
 			var a = result;
 			var l = result[0]["language"];
 			var r = result[0]["isReliable"];
 			console.log(JSON.stringify(a));
 			console.log(l);
 			console.log(r);
-			if(r)msg.channel.send(l);
+			if(r){
+				a.forEach(function(entry){
+					if(entry["code"]==l){
+						msg.channel.send(entry["name"]);
+					}
+				});
+				msg.channel.send(l);
+			}
 		});
 	}
 });
