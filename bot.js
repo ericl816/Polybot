da50eabbf40cc004afbec19debc1d12a7bccb843
jlsajfj <jlsajfj@hotmail.com>
2018-05-26 12:32:00 -0500

    Sentence parsing

--- bot.js
+++ bot.js
@@ -18,6 +18,7 @@ client.on('ready', () => {
 client.on('message', msg => {
 	if(msg.author.id!=client.user.id){
 		detectLanguage.detect(msg.content, function(error, result) {
+			var defaultLang = "";
 			var lang = result[0]["language"];
 			var reliable = result[0]["isReliable"];
 			//console.log(JSON.stringify(a));
@@ -27,13 +28,35 @@ client.on('message', msg => {
 				languages.forEach(function(language){
 					//console.log(entry);
 					if(language["code"]===lang){
-						msg.channel.send(language["name"]);
+						defaultLang=language["name"];
+						//msg.channel.send(language["name"]);
 					}
 				});
 				//msg.channel.send(l);
 			}
+			console.log(msg.content.split(" "));
+			detectLanguage.detect(msg.content.split(" "),function(error,result){
+				console.log(result);
+				result.forEach(function(entry){
+					console.log(entry);
+					var lang = entry[0]["language"];
+					var reliable = entry[0]["isReliable"];
+					if(reliable){
+						languages.forEach(function(language){
+							//console.log(entry);
+							if(language["code"]===lang){
+								msg.channel.send(language["name"]);
+							}
+						});
+					}
+					else{
+						msg.channel.send(defaultLang);
+					}
+					});
+						//msg.channel.send(l);
+				});
 		});
-	}
-});
+		}
+		});
 
 client.login(auth.token);
